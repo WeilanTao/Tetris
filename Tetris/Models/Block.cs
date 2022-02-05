3e5314db92c3d840678bc1c12f613f6d34e10028
WeilanTao <twlemily@hotmail.com>
2022-02-05 05:57:34 -0500

    enable block styling

--- Tetris/Models/Block.cs
+++ Tetris/Models/Block.cs
@@ -18,18 +18,20 @@ namespace Tetris.Models
 
         private bool _isOccupied { get; set; }
 
+        private String _borderColor { get; set; }
         public Block()
         {
 
         }
 
-        public Block(String Color, int x, int y, int Border, bool isOccupied = false)
+        public Block(String Color, int x, int y, int Border,  bool isOccupied = false, String borderColor = "Gray")
         {
             _color = Color;
             _x = x;
             _y = y;
             _border = Border;
             _isOccupied = isOccupied;
+            _borderColor = borderColor;
         }
 
         public String Color { get { return _color; } set { _color = value; } }
@@ -37,7 +39,7 @@ namespace Tetris.Models
         public int X { get { return _x; } set { _x = value; } }
         public int Y { get { return _y; } set { _y = value; } }
         public bool IsOccupied { get { return _isOccupied; } set { _isOccupied = value; } }
-
+        public String BorderColor { get { return _borderColor; } set { _borderColor = value; } }
 
     }
 
